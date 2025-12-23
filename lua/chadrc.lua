52c0b5833cb5db5a3ac9f7c86591280832b779da
gale <v.volkov@hybrid.ai>
2025-12-24 01:27:53 +0300

    transparency

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,8 +6,8 @@
 local M = {}
 
 M.base46 = {
-	theme = "doomchad",
-  transparency = true,
+	theme = "jabuti",
+	transparency = true,
 	-- hl_override = {
 	-- 	Comment = { italic = true },
 	-- 	["@comment"] = { italic = true },
